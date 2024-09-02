1d35962f7a62b52bf2cee34191f1f0ae5f4b505a
julianajacobvareiro <jjacobareiro@gmail.com>
2024-09-02 14:30:58 +0100

    Add project files.

---
+++ ProjetoWebEscola/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoWebEscola.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
